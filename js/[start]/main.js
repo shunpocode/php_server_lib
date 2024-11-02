2f5710b7dfe4b37ec776f72978795057154c9c2e
Maksym <139382157+shunpocode@users.noreply.github.com>
2024-11-02 15:06:44 +0200

    Update main.js

--- js/[start]/main.js
+++ js/[start]/main.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("start");
+console.log(Store.counter.value);
+
+var h1 = document.querySelector('h1');
+var btn1 = document.querySelector('#p');
+var btn2 = document.querySelector('#m');
+
+Store.counter.onUpdate((v) => h1.innerText = v);
+btn1.onclick = () => Store.counter.add()
+btn2.onclick = () => Store.counter.min()
