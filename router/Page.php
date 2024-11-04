1b52d56cae5cb7068ad090134b9bf702c3b74ca2
Maksym <139382157+shunpocode@users.noreply.github.com>
2024-11-04 09:10:43 +0200

    Update Page.php

--- router/Page.php
+++ router/Page.php
@@ -12,7 +12,6 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
-	<!-- <link rel="stylesheet" href="css/main.css"> -->
 	<link rel="stylesheet" href="<?php echo $css; ?>">
 	<script src="<?php echo $javascript; ?>" async></script>
 </head>
